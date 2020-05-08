ecc6e081a1caa10727fd3af0dbefd76c7f3fee1f
rotexdegba <rotimi@w-edm-192-139-007-217.nofc.cfs.nrcan.gc.ca>
2020-05-07 21:59:47 -0600

    Code clean up

--- public/js/app.js
+++ public/js/app.js
@@ -3,7 +3,7 @@
 
         $('.sidenav').sidenav();
         $('.tooltipped').tooltip();
-        $('.collapsible').collapsible({accordion: false});
+        
 
     }); // end of document ready
 })(jQuery); // end of jQuery name space

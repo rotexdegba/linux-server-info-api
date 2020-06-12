9228a7338d88062f1e6fa2986df628892ae2afd7
rotexdegba <rotimi@w-edm-192-139-007-217.nofc.cfs.nrcan.gc.ca>
2020-06-11 20:24:08 -0600

    Code clean up and implementation of system_overview_schema in progress

--- src/controllers/AppBase.php
+++ src/controllers/AppBase.php
@@ -888,50 +888,52 @@ class AppBase extends \Slim3MvcTools\Controllers\BaseController
         // Total number of running processes (linux only)
         ////////////////////////////////////////////////////////////////////////
         $systemOverviewData['system_overview_schema']['total_number_of_running_processes_linux'] = 
-            Utils::getValIfTrueOrGetDefault(
+            
                 is_array($linfoObj->getProcessStats()) && array_key_exists('totals', $linfoObj->getProcessStats())
-                && is_array($linfoObj->getProcessStats()['totals']) && array_key_exists('running', $linfoObj->getProcessStats()['totals']),
-                    
-                (int)$linfoObj->getProcessStats()['totals']['running'], 
+                && is_array($linfoObj->getProcessStats()['totals']) && array_key_exists('running', $linfoObj->getProcessStats()['totals'])
+                ?  
+                (int)$linfoObj->getProcessStats()['totals']['running']
+                : 
                 -1
-            );
+            ;
         
         ////////////////////////////////////////////////////////////////////////
         // Total number of sleeping processes (linux only)
         ////////////////////////////////////////////////////////////////////////
         $systemOverviewData['system_overview_schema']['total_number_of_sleeping_processes_linux'] = 
-            Utils::getValIfTrueOrGetDefault( 
                     
                 is_array($linfoObj->getProcessStats()) && array_key_exists('totals', $linfoObj->getProcessStats())
-                && is_array($linfoObj->getProcessStats()['totals']) && array_key_exists('sleeping', $linfoObj->getProcessStats()['totals']),
-                    
-                (int)$linfoObj->getProcessStats()['totals']['sleeping'], 
-                -1
-            );
+                && is_array($linfoObj->getProcessStats()['totals']) && array_key_exists('sleeping', $linfoObj->getProcessStats()['totals'])
+                ?
+                (int)$linfoObj->getProcessStats()['totals']['sleeping']
+                :
+                -1;
         
         ////////////////////////////////////////////////////////////////////////
         // Total number of stopped processes (linux only)
         ////////////////////////////////////////////////////////////////////////
         $systemOverviewData['system_overview_schema']['total_number_of_stopped_processes_linux'] = 
-            Utils::getValIfTrueOrGetDefault(
+            
                 is_array($linfoObj->getProcessStats()) && array_key_exists('totals', $linfoObj->getProcessStats())
-                && is_array($linfoObj->getProcessStats()['totals']) && array_key_exists('stopped', $linfoObj->getProcessStats()['totals']),
-                    
-                (int)$linfoObj->getProcessStats()['totals']['stopped'], 
+                && is_array($linfoObj->getProcessStats()['totals']) && array_key_exists('stopped', $linfoObj->getProcessStats()['totals'])
+                ?
+                (int)$linfoObj->getProcessStats()['totals']['stopped']
+                :
                 -1
-            );
+            ;
         
         ////////////////////////////////////////////////////////////////////////
         // Total number of zombie processes (linux only)
         ////////////////////////////////////////////////////////////////////////
         $systemOverviewData['system_overview_schema']['total_number_of_zombie_processes_linux'] = 
-            Utils::getValIfTrueOrGetDefault(
+            
                 is_array($linfoObj->getProcessStats()) && array_key_exists('totals', $linfoObj->getProcessStats())
-                && is_array($linfoObj->getProcessStats()['totals']) && array_key_exists('zombie', $linfoObj->getProcessStats()['totals']),
-                    
-                (int)$linfoObj->getProcessStats()['totals']['zombie'], 
+                && is_array($linfoObj->getProcessStats()['totals']) && array_key_exists('zombie', $linfoObj->getProcessStats()['totals'])
+                ?
+                (int)$linfoObj->getProcessStats()['totals']['zombie']
+                :
                 -1
-            );
+            ;
         
         ////////////////////////////////////////////////////////////////////////
         // Number of active users

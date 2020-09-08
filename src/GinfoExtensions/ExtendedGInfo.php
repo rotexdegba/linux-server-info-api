3bf46aac0ff84791cc46973bf42687c4affe5e32
rotexdegba <rotimi@w-edm-192-139-007-217.nofc.cfs.nrcan.gc.ca>
2020-09-08 11:39:28 -0600

    Switched from \ExtendedWindows to \Ginfo\OS\Windows to see if it will fix issues on Windows.

--- src/GinfoExtensions/ExtendedGInfo.php
+++ src/GinfoExtensions/ExtendedGInfo.php
@@ -5,8 +5,16 @@ class ExtendedGInfo extends \Ginfo\Ginfo {
     public function __construct() {
         
         if ('\\' === \DIRECTORY_SEPARATOR) {
-            $this->os = new \ExtendedWindows();
+            //$this->os = new \ExtendedWindows(); // Seems like this sub-class which
+                                                  // currently contains no overriden
+                                                  // code, is not allowing powershell
+                                                  // scripts to be found because it 
+                                                  // breaks the file location logic
+                                                  // in its parent class.
+            $this->os = new \Ginfo\OS\Windows();
+            
         } else {
+            
             // bsd, linux, darwin, solaris
             $this->os = new \ExtendedLinux();
         }

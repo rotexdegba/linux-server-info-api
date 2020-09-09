b72263f237bced77d08c5f4ccf7fe17f844ef376
Rotimi Ade <rotexdegba007-github@yahoo.ca>
2020-09-09 11:34:03 -0600

    Switched from \Ginfo\OS\Windows back to \ExtendedWindows

--- src/GinfoExtensions/ExtendedGInfo.php
+++ src/GinfoExtensions/ExtendedGInfo.php
@@ -5,13 +5,7 @@ class ExtendedGInfo extends \Ginfo\Ginfo {
     public function __construct() {
         
         if ('\\' === \DIRECTORY_SEPARATOR) {
-            //$this->os = new \ExtendedWindows(); // Seems like this sub-class which
-                                                  // currently contains no overriden
-                                                  // code, is not allowing powershell
-                                                  // scripts to be found because it 
-                                                  // breaks the file location logic
-                                                  // in its parent class.
-            $this->os = new \Ginfo\OS\Windows();
+            $this->os = new \ExtendedWindows();
             
         } else {
             

64b70cbb42046f153e8b1ebcab464407a46d54f5
Rotimi Ade <rotexdegba007-github@yahoo.ca>
2020-09-10 13:02:38 -0600

    Switched from \Ginfo\OS\Windows back to \ExtendedWindows

--- src/GinfoExtensions/ExtendedWindows.php
+++ src/GinfoExtensions/ExtendedWindows.php
@@ -2,5 +2,24 @@
 
 class ExtendedWindows extends \Ginfo\OS\Windows {
     
-}
+    public function __construct()
+    {
+        ini_set('max_execution_time', 0);
+        parent::__construct();
+    }
 
+    protected function getInfo(string $name): ?array
+    {
+//        if (\array_key_exists($name, $this->infoCache)) {
+//            
+//            return $this->infoCache[$name];
+//        }
+
+        $result = \json_decode(
+            shell_exec('chcp 65001 | powershell -file '. S3MVC_APP_ROOT_PATH.'\\vendor\\gemorroj\\ginfo\\bin\\windows\\'.$name.'.ps1'), 
+            true
+        );
+
+        return \is_scalar($result) ? [$result] : $result;
+    }
+}

39905398092ed91e78199176ee471359135d254c
Rotimi Ade <rotexdegba007-github@yahoo.ca>
2020-09-14 14:58:15 -0600

    Added favicons

--- src/layout-templates/main-template.php
+++ src/layout-templates/main-template.php
@@ -5,6 +5,17 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
         <title>Server Info API</title>
 
+        <!-- fav and touch icons - thanks to https://realfavicongenerator.net -->
+        <link rel="apple-touch-icon" sizes="180x180" href="<?= s3MVC_MakeLink('/images/favicons/apple-touch-icon.png'); ?>">
+        <link rel="icon" type="image/png" sizes="32x32" href="<?= s3MVC_MakeLink('/images/favicons/favicon-32x32.png'); ?>">
+        <link rel="icon" type="image/png" sizes="16x16" href="<?= s3MVC_MakeLink('/images/favicons/favicon-16x16.png'); ?>">
+        <link rel="manifest" href="<?= s3MVC_MakeLink('/images/favicons/site.webmanifest'); ?>">
+        <link rel="mask-icon" href="<?= s3MVC_MakeLink('/images/favicons/safari-pinned-tab.svg'); ?>" color="#5bbad5">
+        <link rel="shortcut icon" href="<?= s3MVC_MakeLink('/images/favicons/favicon.ico'); ?>">
+        <meta name="msapplication-TileColor" content="#da532c">
+        <meta name="msapplication-config" content="<?= s3MVC_MakeLink('/images/favicons/browserconfig.xml'); ?>">
+        <meta name="theme-color" content="#ffffff">
+
         <!--Import Google Icon Font-->
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
